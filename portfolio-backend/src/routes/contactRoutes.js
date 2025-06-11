eb0d89cec48ae362c264d74a24469d60818f1757
shrestha-sachin <sachinstha600@gmail.com>
2025-06-11 10:34:43 -0500

    deleted backend

--- portfolio-backend/src/routes/contactRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const contactController = require('../controllers/contactController');
-
-// POST /api/contact (note: '/api' is already prefixed in app.js)
-router.post('/contact', contactController.submitContactForm.bind(contactController));
-
-module.exports = router;
--- portfolio-backend/src/routes/newsletterRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const newsletterController = require('../controllers/newsletterController');
-
-// POST /api/newsletter
-router.post('/newsletter', newsletterController.subscribe);
-
-module.exports = router;
